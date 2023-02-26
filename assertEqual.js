1629b83a714bd29c48a20dba8a0128767754bcb5
Saim Amir <saimxamir@gmail.com>
2023-02-25 20:33:47 -0500

    add assertEqual to lotide

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟩🟩🟩 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
