81c55498c5b66011b85fdb8ea4154a3b3ee4d429
Saim Amir <saimxamir@gmail.com>
2023-02-26 22:23:31 -0500

    used eslint on assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length === 0 && array2.length === 0) { //this targets case where empty strings are evaluated.
     return true;
   } else if (array1.length === array2.length) {
@@ -6,23 +6,23 @@ const eqArrays = function (array1, array2) {
       if (array1[i] === array2[i]) {
         return true;
       } else {
-        return false
+        return false;
       }
     }
   } else {
-    return false
+    return false;
   }
-}
+};
 
-const assertArraysEqual = function (array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log("Array 1 is equal to Array 2.");
   } else {
     console.log("Array 1 is not equal to Array 2.");
   }
-}
+};
 
-// test cocde
+// test code
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // equal
 assertArraysEqual([1, 2, 3], [1, 2]); // not equal
