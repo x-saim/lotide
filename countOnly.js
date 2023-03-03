dacb37bad2d1cfa48b9a2ee6539cfef0e24ed63a
Saim Amir <saimxamir@gmail.com>
2023-03-02 23:23:22 -0500

    add countOnly.js function for only counting all items

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function will be given an array and an object (2 parameters)
+// function to return an object containing counts of everything
+// that the input object listed. only keys with truthy value will be counted.
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function (allItems, itemsToCount) {
+  let results = {};
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+  return results;
+}
+
+// test code
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+console.log(result1);
