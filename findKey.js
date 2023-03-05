241d54dd69b64c8756486d70772168d465791a2f
Saim Amir <saimxamir@gmail.com>
2023-03-05 17:00:25 -0500

    add findKey() function to lotide with assertations

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  const keysOfObject = Object.keys(object);
+  for (let e of keysOfObject) {
+    if (callback(object[e])) {
+      return e;
+    }
+  }
+  return undefined;
+};
+
+
+
+const myObj1 = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+
+findKey(myObj1, x => x.stars === 2); // => "noma"
+
+
+assertEqual(findKey(myObj1, x => x.stars === 2), "noma");
+assertEqual(findKey(myObj1, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(myObj1, x => x.stars === 4), undefined);
