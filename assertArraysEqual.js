06b0431e2a2a853944d609fdef3ffa9dc1469341
Saim Amir <saimxamir@gmail.com>
2023-03-17 00:32:42 -0400

    condense code using ternary operator

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,8 @@
 const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log("Array 1 is equal to Array 2.");
-  } else {
-    console.log("Array 1 is not equal to Array 2.");
-  }
+  //using ternary operator instead of previously committed if-else statement.
+  eqArrays(array1, array2) ? console.log("Array 1 is equal to Array 2.") : console.log("Array 1 is not equal to Array 2.");
 };
 
 module.exports = assertArraysEqual;
